87f49fa5445c5808d538d7c73358a774ec25b9ba
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-02-04 12:43:13 +0100

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,33 @@
+package com.cc.java;
+public class App {
+
+    // static Honeybee worker;
+
+    public static void main(String[] args) {
+      
+        HoneyBee queen = new Queen();
+        HoneyBee drone = new Drone();
+        HoneyBee worker = new Worker();
+
+        output(queen.doYourJob());
+        output(drone.doYourJob());
+        output(worker.doYourJob());
+
+        output("-----------------");
+
+        output(queen.fly());
+        output(drone.fly());
+        output(worker.fly());
+
+        output("-----aber: -----");
+
+        // Das sollte eigentlich nicht gehen!
+        HoneyBee bee = new HoneyBee();
+        output(bee.doYourJob());
+  
+    }
+
+    private static void output(String outputStr) {
+        System.out.println(outputStr);
+    }
+}
